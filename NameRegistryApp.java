26639c9d6643d620128607993102a625838424c2
azast <petteri.hokka@icloud.com>
2022-11-15 13:40:24 +0200

    Object oriented design: 2

--- NameRegistryApp.java
+++ NameRegistryApp.java
@@ -20,5 +20,6 @@ public class NameRegistryApp
     String[] names = ui.readNames (); // read names from user
     nameRegister.setNames (names); // store names into register
     ui.showNames (nameRegister.getNames ()); // show names in register to user
+    //köo6kyh
   }
 }
